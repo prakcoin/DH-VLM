1203038ba6cac5d9aaa0f24a36c60598fda052b5
prakcoin <prakhar2@gmail.com>
2026-03-30 17:04:22 -0500

    Added initial new agent evaluation via. Strands

--- evaluation/agent_eval.py
+++ evaluation/agent_eval.py
@@ -1,116 +1,54 @@
-import os
-import uuid
-import json
+from strands import Agent
+from strands_evals import Case, Experiment, ActorSimulator
+from strands_evals.evaluators import HelpfulnessEvaluator, GoalSuccessRateEvaluator, FaithfulnessEvaluator, ToolSelectionAccuracyEvaluator 
+from strands_evals.mappers import StrandsInMemorySessionMapper
+from strands_evals.telemetry import StrandsEvalsTelemetry
 import sys
-import pandas as pd
+import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.agent import DHAgent
-from langchain_aws import ChatBedrock, BedrockEmbeddings
-from langchain_core.messages import SystemMessage, HumanMessage
-from ragas.embeddings import LangchainEmbeddingsWrapper
-from ragas.llms import LangchainLLMWrapper
-from ragas.metrics import (
-    FactualCorrectness, 
-    SemanticSimilarity
-)
-from ragas.dataset_schema import SingleTurnSample, EvaluationDataset
-from ragas import evaluate
 
-agent = DHAgent()
-bedrock_llm = ChatBedrock(model_id="us.amazon.nova-pro-v1:0", region_name="us-east-1")
-evaluator_llm = LangchainLLMWrapper(bedrock_llm)
-bedrock_embeddings = BedrockEmbeddings(model_id="amazon.nova-2-multimodal-embeddings-v1:0", region_name="us-east-1")
-embeddings = LangchainEmbeddingsWrapper(bedrock_embeddings)
+from src.orchestration.orchestrator import Orchestrator
 
-EVAL_MODE = "general"
-INPUT_FILE = f"datasets/eval_{EVAL_MODE}.json"
-baseline_system_prompt = """Role: 
-    You are the lead archival assistant for the Dior Homme Autumn/Winter 2004 "Victim of the Crime" collection. Your goal is to provide precise and factual information regarding this specific collection.
+telemetry = StrandsEvalsTelemetry().setup_in_memory_exporter()
+memory_exporter = telemetry.in_memory_exporter
 
-    Archival Guardrails:
-    Every sentence must be in standard sentence case (e.g., 'The jacket is leather,' not 'THE JACKET IS LEATHER' or 'The Jacket Is Leather').
-    Use a neutral, professional, editorial tone. Avoid marketing language.
-    All look-based data MUST be sorted by Look Number in ascending order (1, 2, 3...).
-    When summarizing motifs, you must distinguish between "Baseline Attributes" and "Collection-Specific Design Language." Do not report "Solid pattern," "Cotton," or "Buttons" as recurring motifs unless specifically asked. These are considered functional/ubiquitous and do not constitute a motif for this collection. Only report a recurring element if it serves as a stylistic signature. A "belt" is a utility; a "belt with onyx studs" is a motif. If an attribute appears in nearly every look (like "Solid"), it is a baseline characteristic and should be omitted from a summary of motifs.
-    When multiple results refer to the same garment, consolidate them into a single entry. List the item name once, followed by all look numbers where it appears in parentheses. (e.g., "Leather studded belt (Looks 5, 7, 9, 12, 16...)").
+def get_response(case: Case) -> str:
+    agent = Orchestrator()
+    agent.agent.trace_attributes = {
+        "gen_ai.conversation.id": case.session_id,
+        "session.id": case.session_id
+    },
+    response = agent.ask(case.input)
 
-    Output Format:
-    Your response must contain only the direct answer to the user's query. If the user asks for "Look Numbers," do not provide "Materials." If the user asks for "Items," do not provide "Reference Codes." Every extra word of metadata is a failure of the archival protocol.
-    Never list the exact same item name more than once in a single response. Use a comma-separated list for look numbers to keep the response concise and professional."""
+    finished_spans = memory_exporter.get_finished_spans()
+    mapper = StrandsInMemorySessionMapper()
+    session = mapper.map_to_session(finished_spans, session_id=case.session_id)
 
-def invokeAgent(query, session_id):
-    full_text = ""
-    try:
-        for chunk in agent.invoke(query, session_id):
-            full_text += chunk
-        return full_text if full_text else "Agent failed to provide a response."
-    except Exception as e:
-        return f"Error: {str(e)}"
+    return {"output": str(response), "trajectory": session}
 
-def invokeBaseline(query):
-    messages = [
-        SystemMessage(content=baseline_system_prompt),
-        HumanMessage(content=query)
-    ]
-    
-    response = bedrock_llm.invoke(messages)
-    
-    return response.content
+evaluators = [
+    HelpfulnessEvaluator(),
+    FaithfulnessEvaluator(),
+]
 
-with open(INPUT_FILE, 'r') as f:
-    EVAL_DATA = json.load(f)
+test_cases = [
+    Case[str, str](
+        input="What does look 1 consist of?",
+        expected_output="Look 1 consists of a 1B blazer, a leather tie, a pinstripe shirt, trousers, suede moto boots, aviator sunglasses, a leather belt, and a bandana bracelet.",
+    ),
+    # Case[str, str](
+    #     input="What is the reference code for the beetle leather jacket?",
+    #     expected_output="The reference code for the beetle leather jacket is 4HH5041101.",
+    # ),
+    # Case[str, str](
+    #     input="What material is the jacket in look 2 made from?",
+    #     expected_output="The jacket in look 2 is made from leather, more specifically calfskin.",
+    # )
+]
 
-metrics_config = {
-    "general": [FactualCorrectness(llm=evaluator_llm)],
-    "aggregation": [FactualCorrectness(llm=evaluator_llm)],
-    "vqa": [SemanticSimilarity(embeddings=embeddings)],
-    "outofscope": [FactualCorrectness(llm=evaluator_llm)],
-    "search": [SemanticSimilarity(embeddings=embeddings)]
-}
+experiment = Experiment[str, str](cases=test_cases, evaluators=evaluators)
+reports = experiment.run_evaluations(get_response)
 
-mode_metrics = metrics_config.get(EVAL_MODE)
-
-agent_samples = []
-agent_answers = []
-baseline_samples = []
-baseline_answers = []
-
-for conversation in EVAL_DATA:
-    session_id = str(uuid.uuid4())
-
-    query = conversation["query"]
-    reference = conversation["reference"]
-    topics = conversation.get("reference_topics", []) 
-    
-    agent_answer = invokeAgent(query, session_id)
-    baseline_answer = invokeBaseline(query)
-    
-    agent_samples.append(SingleTurnSample(
-        user_input=query,
-        response=agent_answer,
-        reference=reference
-    ))
-
-    agent_answers.append(agent_answer)
-    baseline_answers.append(baseline_answer)
-
-    print(f"[{EVAL_MODE}] Ran turn: {query[:40]}...")
-
-os.makedirs("results", exist_ok=True)
-
-dataset = EvaluationDataset(samples=agent_samples)
-results = evaluate(dataset=dataset, metrics=mode_metrics)
-results.to_pandas().to_csv(f"results/{EVAL_MODE}_results.csv", index=False)
-
-df = pd.read_csv(f"results/{EVAL_MODE}_results.csv")
-
-metric_name = df.columns[-1]
-average_score = df[metric_name].mean()
-
-print(f"Metric: {metric_name}")
-print(f"Average: {average_score:.4f}")
-print(f"Done. Saved results for {EVAL_MODE}.")
-
-for agent_answer, baseline_answer in zip(agent_answers, baseline_answers):
-    print("Agent Answer:\n", agent_answer)
-    print("Baseline Answer:\n", baseline_answer)
+print("=== Basic Output Evaluation Results ===")
+reports[0].run_display()
